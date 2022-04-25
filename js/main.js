84cde2a6effeab3331c2a8b32fab8b2b1cf90315
Jean Silva <jeanslucasilva@gmail.com>
2022-04-25 13:05:12 -0300

    time tracking feature

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const partialProgress = document.querySelector("#partial-progress");
 const textForm = document.querySelector("#text-form");
 const url = document.querySelector("#url");
 const title = document.querySelector("#title");
+const currentTime = document.querySelector("#current");
+const totalTime = document.querySelector("#total");
 textForm.addEventListener("submit", (event) => handleSubmit(event));
 let interval = 0;
 
@@ -12,11 +14,43 @@ audioPlayer.addEventListener("error", () => {
     alert('Invalid URL');
 });
 
+audioPlayer.addEventListener("loadedmetadata", () => {
+    if (audioPlayer.duration !== Infinity) {
+        const minutes = Math.floor(audioPlayer.duration / 60);
+        const seconds = (audioPlayer.duration - minutes * 60).toFixed().padStart(2, '0');
+        totalTime.innerHTML = `${minutes}:${seconds}`;
+    }
+})
+
+function getDuration(src, cb) {
+    var audio = new Audio();
+    audioPlayer.onloadedmetadata = () => {
+        cb(audio.duration);
+    }
+}
+
+getDuration("banana", (length) => {
+    console.log(length)
+});
+
 function play() {
     if (audioPlayer.paused) {
+        const initialURL = audioPlayer.getAttribute('src');
+        if (url.value === '') {
+            title.innerHTML = `<h2>${initialURL.split('/').pop()}</h2>`;
+            audioPlayer.setAttribute('src', initialURL);
+        }
+        /*
+        const totalMinutes = Math.floor(audioPlayer.duration / 60);
+        const totalSeconds = (audioPlayer.duration - totalMinutes * 60).toFixed().padStart(2, '0');
+        totalTime.innerHTML = `${totalMinutes} : ${totalSeconds}`;
+        */
         interval = setInterval(() => {
             const progress = Math.floor((audioPlayer.currentTime / audioPlayer.duration) * 100);
             partialProgress.style.width = `${progress}%`;
+            const minutes = Math.floor(audioPlayer.currentTime / 60);
+            const seconds = (audioPlayer.currentTime - minutes * 60).toFixed().padStart(2, '0');
+            currentTime.innerHTML = `${minutes}:${seconds}`
         }, 100);
         playButton.innerHTML = "<i class=\"fa-solid fa-pause\"></i>";
         audioPlayer.play();
@@ -55,4 +89,5 @@ function handleSubmit(e) {
     e.preventDefault();
     title.innerHTML = `<h2>${url.value.split("/").pop()}</h2>`;
     audioPlayer.setAttribute('src', url.value);
+    play();
 }
